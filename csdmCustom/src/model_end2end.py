81b55f4bdf8ac94fd83ab61d64a03b8aa85dc100
yu_yeoul <yu_yeoul@naver.com>
2024-04-03 03:02:14 +0900

    fix

--- csdmCustom/src/model_end2end.py
+++ csdmCustom/src/model_end2end.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 import torch.nn.functional as F
 import numpy as np
-from unet import UNet, UNet_v2
+from csdmCustom.src.unet import UNet, UNet_v2
 from torchvision import models
 
 
